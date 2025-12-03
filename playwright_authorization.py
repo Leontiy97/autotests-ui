94b156da07ea8f9e2a3247bac1d0363f8dc8b9fd
Leon <kbjytkm1@gmail.com>
2025-12-03 22:46:19 +0300

    Add Playwright script

---
+++ playwright_authorization.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
+    email_input.fill("user.name@gmail.com")
+
+    password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
+    password_input.fill("password")
+
+    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button.click()
+
+    check_validation_text = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    expect(check_validation_text).to_be_visible()
+    expect(check_validation_text).to_have_text("Wrong email or password")
+
+    page.wait_for_timeout(5000)
+
