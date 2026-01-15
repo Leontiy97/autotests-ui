3c5fab5653ea7943e207316e1ecebc4ac9ebf4a2
Leon <kbjytkm1@gmail.com>
2026-01-15 20:49:18 +0300

    add script_courses

--- playwright_registration.py
+++ playwright_registration.py
@@ -31,3 +31,12 @@ with sync_playwright() as playwright:
     context.storage_state(path="browser-state.json")
 
     # page.wait_for_timeout(1000)
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
